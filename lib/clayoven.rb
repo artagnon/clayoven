7c7451c9c7981ba629d3c536cfc750792ded2075
Rohan Jain <crodjer@gmail.com>
2013-02-17 19:13:21 +0530

    Introduce ClayOven namespace
    To prevent collisions with any other modules in global
    namespace. For instance the module `Mail` collides
    with the ruby library `mail`.
    
    Signed-off-by: Rohan Jain <crodjer@gmail.com>

--- lib/clayoven.rb
+++ lib/clayoven.rb
@@ -15,87 +15,89 @@ def when_introduced(filename)
   end
 end
 
-module Core
-  class Page
-    attr_accessor :filename, :permalink, :timestamp, :title, :topic, :body,
-    :paragraphs, :target, :indexfill, :topics
+module ClayOven
+    module Core
+        class Page
+            attr_accessor :filename, :permalink, :timestamp, :title, :topic, :body,
+            :paragraphs, :target, :indexfill, :topics
 
-    def render(topics)
-      @topics = topics
-      @paragraphs = ClayText.process! @body
-      Slim::Engine.set_default_options pretty: true, sort_attrs: false
-      rendered = Slim::Template.new { IO.read("design/template.slim") }.render(self)
-      File.open(@target, mode="w") { |targetio|
-        nbytes = targetio.write(rendered)
-        puts "[GEN] #{@target} (#{nbytes} bytes out)"
-      }
+            def render(topics)
+                @topics = topics
+                @paragraphs = ClayText.process! @body
+                Slim::Engine.set_default_options pretty: true, sort_attrs: false
+                rendered = Slim::Template.new { IO.read("design/template.slim") }.render(self)
+                File.open(@target, mode="w") { |targetio|
+                    nbytes = targetio.write(rendered)
+                    puts "[GEN] #{@target} (#{nbytes} bytes out)"
+                }
+            end
+        end
+
+        class IndexPage < Page
+            def initialize(filename)
+                @filename = filename
+                if @filename == "index"
+                    @permalink = @filename
+                else
+                    @permalink = filename.split(".index")[0]
+                end
+                @topic = @permalink
+                @target = "#{@permalink}.html"
+                @timestamp = when_introduced @filename
+            end
+        end
+
+        class ContentPage < Page
+            attr_accessor :pub_date
+
+            def initialize(filename)
+                @filename = filename
+                @topic, @permalink = @filename.split(":", 2)
+                @target = "#{@permalink}.html"
+                @indexfill = nil
+                @timestamp = when_introduced @filename
+            end
+        end
+
+        def self.main
+            abort "error: index file not found; aborting" if not File.exists? "index"
+
+            config = ConfigData.new
+            all_files = (Dir.entries(".") -
+                         [".", "..", ".clayoven", "design"]).reject { |entry|
+                config.ignore.any? { |pattern| %r{#{pattern}} =~ entry }
+            }
+
+            abort "error: design/template.slim file not found; aborting" if not
+                Dir.entries("design").include? "template.slim"
+
+            index_files = ["index"] + all_files.select { |file| /\.index$/ =~ file }
+            content_files = all_files - index_files
+            topics = index_files.map { |file| file.split(".index")[0] }.uniq
+
+            # Next, look for stray files
+            (content_files.reject { |file| topics.include? (file.split(":", 2)[0]) })
+                .each { |stray_entry|
+                content_files = content_files - [stray_entry]
+                puts "warning: #{stray_entry} is a stray file or directory; ignored"
+            }
+
+            index_pages = index_files.map { |filename| IndexPage.new(filename) }
+            content_pages = content_files.map { |filename| ContentPage.new(filename) }
+
+            # First, fill in all the page attributes
+            (index_pages + content_pages).each { |page|
+                page.title, page.body = (IO.read page.filename).split("\n\n", 2)
+            }
+
+            # Compute the indexfill for indexes
+            topics.each { |topic|
+                topic_index = index_pages.select { |page| page.topic == topic }[0]
+                topic_index.indexfill = content_pages.select { |page|
+                    page.topic == topic }.sort { |a, b| b.timestamp <=> a.timestamp }
+            }
+
+            (index_pages + content_pages).each { |page| page.render topics }
+        end
     end
-  end
-
-  class IndexPage < Page
-    def initialize(filename)
-      @filename = filename
-      if @filename == "index"
-        @permalink = @filename
-      else
-        @permalink = filename.split(".index")[0]
-      end
-      @topic = @permalink
-      @target = "#{@permalink}.html"
-      @timestamp = when_introduced @filename
-    end
-  end
-
-  class ContentPage < Page
-    attr_accessor :pub_date
-
-    def initialize(filename)
-      @filename = filename
-      @topic, @permalink = @filename.split(":", 2)
-      @target = "#{@permalink}.html"
-      @indexfill = nil
-      @timestamp = when_introduced @filename
-    end
-  end
-
-  def self.main
-    abort "error: index file not found; aborting" if not File.exists? "index"
-
-    config = ConfigData.new
-    all_files = (Dir.entries(".") -
-                 [".", "..", ".clayoven", "design"]).reject { |entry|
-      config.ignore.any? { |pattern| %r{#{pattern}} =~ entry }
-    }
-
-    abort "error: design/template.slim file not found; aborting" if not
-      Dir.entries("design").include? "template.slim"
-
-    index_files = ["index"] + all_files.select { |file| /\.index$/ =~ file }
-    content_files = all_files - index_files
-    topics = index_files.map { |file| file.split(".index")[0] }.uniq
-
-    # Next, look for stray files
-    (content_files.reject { |file| topics.include? (file.split(":", 2)[0]) })
-      .each { |stray_entry|
-      content_files = content_files - [stray_entry]
-      puts "warning: #{stray_entry} is a stray file or directory; ignored"
-    }
-
-    index_pages = index_files.map { |filename| IndexPage.new(filename) }
-    content_pages = content_files.map { |filename| ContentPage.new(filename) }
-
-    # First, fill in all the page attributes
-    (index_pages + content_pages).each { |page|
-      page.title, page.body = (IO.read page.filename).split("\n\n", 2)
-    }
-
-    # Compute the indexfill for indexes
-    topics.each { |topic|
-      topic_index = index_pages.select { |page| page.topic == topic }[0]
-      topic_index.indexfill = content_pages.select { |page|
-        page.topic == topic }.sort { |a, b| b.timestamp <=> a.timestamp }
-    }
-
-    (index_pages + content_pages).each { |page| page.render topics }
-  end
 end
--- lib/clayoven/claytext.rb
+++ lib/clayoven/claytext.rb
@@ -1,72 +1,74 @@
-class Paragraph
-  attr_accessor :content, :first, :type
+module ClayOven
+    class Paragraph
+        attr_accessor :content, :first, :type
 
-  def initialize(content)
-    @content = content
-    @first = false
-    @type = :plain
-  end
+        def initialize(content)
+            @content = content
+            @first = false
+            @type = :plain
+        end
 
-  def is_first?
-    @first
-  end
-end
-
-module ClayText
-  def self.process!(body)
-    htmlescape_rules = {
-      "&" => "&amp;",
-      "\"" => "&quot;",
-      "'" => "&#39;",
-      "<" => "&lt;",
-      ">" => "&gt;"
-    }.freeze
-
-    paragraph_types = [:plain, :emailquote, :codeblock, :header, :footer]
-    paragraph_rules = {
-      Proc.new { |line| line.start_with? "&gt; " } => lambda { |paragraph|
-        paragraph.type = :emailquote },
-      Proc.new { |line| line.start_with? "    " } => lambda { |paragraph|
-        paragraph.type = :codeblock },
-      Proc.new { |line| /^\[\d+\]: / =~ line } => lambda { |paragraph|
-        paragraph.content.gsub!(%r{^(\[\d+\]:) (.*://(.*))}) {
-          "#{$1} <a href=\"#{$2}\">#{$3[0, 64]}#{%{...} if $3.length > 67}</a>"
-        }
-        paragraph.type = :footer
-      }
-    }.freeze
-
-    # First, htmlescape the body text
-    body.gsub!(/[&"'<>]/, htmlescape_rules)
-
-    paragraphs = []
-    body.split("\n\n").each { |content|
-      paragraphs << Paragraph.new(content)
-    }
-
-    paragraphs[0].first = true
-    if paragraphs[0].content.start_with? "(" and
-        paragraphs[0].content.end_with? ")"
-      paragraphs[0].type = :header
+        def is_first?
+            @first
+        end
     end
 
-    # Paragraph-level processing
-    paragraphs.each { |paragraph|
-      paragraph_rules.each { |proc_match, lambda_cb|
-        if paragraph.content.lines.all? &proc_match
-          lambda_cb.call paragraph
+    module ClayText
+        def self.process!(body)
+            htmlescape_rules = {
+                "&" => "&amp;",
+                "\"" => "&quot;",
+                "'" => "&#39;",
+                "<" => "&lt;",
+                ">" => "&gt;"
+            }.freeze
+
+            paragraph_types = [:plain, :emailquote, :codeblock, :header, :footer]
+            paragraph_rules = {
+                Proc.new { |line| line.start_with? "&gt; " } => lambda { |paragraph|
+                    paragraph.type = :emailquote },
+                Proc.new { |line| line.start_with? "    " } => lambda { |paragraph|
+                    paragraph.type = :codeblock },
+                Proc.new { |line| /^\[\d+\]: / =~ line } => lambda { |paragraph|
+                    paragraph.content.gsub!(%r{^(\[\d+\]:) (.*://(.*))}) {
+                        "#{$1} <a href=\"#{$2}\">#{$3[0, 64]}#{%{...} if $3.length > 67}</a>"
+                    }
+                    paragraph.type = :footer
+                }
+            }.freeze
+
+            # First, htmlescape the body text
+            body.gsub!(/[&"'<>]/, htmlescape_rules)
+
+            paragraphs = []
+            body.split("\n\n").each { |content|
+                paragraphs << Paragraph.new(content)
+            }
+
+            paragraphs[0].first = true
+            if paragraphs[0].content.start_with? "(" and
+                    paragraphs[0].content.end_with? ")"
+                paragraphs[0].type = :header
+            end
+
+            # Paragraph-level processing
+            paragraphs.each { |paragraph|
+                paragraph_rules.each { |proc_match, lambda_cb|
+                    if paragraph.content.lines.all? &proc_match
+                        lambda_cb.call paragraph
+                    end
+                }
+            }
+
+            # Generate is_*? methods for Paragraph
+            Paragraph.class_eval {
+                paragraph_types.each { |type|
+                    define_method("is_#{type.to_s}?") { @type == type }
+                }
+            }
+
+            body = paragraphs.map(&:content).join("\n\n")
+            paragraphs
         end
-      }
-    }
-
-    # Generate is_*? methods for Paragraph
-    Paragraph.class_eval {
-      paragraph_types.each { |type|
-        define_method("is_#{type.to_s}?") { @type == type }
-      }
-    }
-
-    body = paragraphs.map(&:content).join("\n\n")
-    paragraphs
-  end
+    end
 end
