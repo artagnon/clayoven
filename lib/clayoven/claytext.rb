artagnon/clayoven

commit 8b0cea8523170f2c6c6934e3784c97f5a36dfeec

*: comments along with minor cleanup

lib/clayoven/claytext.rb
@@ -1,4 +1,10 @@
 module ClayText
+  
+  # A paragraph of text
+  #
+  # :content contains its content
+  # :fist asserts whether it's the first paragraph in the body
+  # :type can be one of paragraph_types (defined later)
   class Paragraph
     attr_accessor :content, :first, :type
 
@@ -7,13 +13,15 @@ module ClayText
       @first = false
       @type = :plain
     end
-
-    def is_first?
-      @first
-    end
   end
 
+  # Takes a body of claytext, breaks it up into paragraphs, and
+  # applies various rules on it.
+  #
+  # Returns a list of Paragraphs
   def self.process!(body)
+
+    # see: http://php.net/manual/en/function.htmlspecialchars.php
     htmlescape_rules = {
       "&" => "&amp;",
       "\"" => "&quot;",
@@ -22,35 +30,56 @@ module ClayText
       ">" => "&gt;"
     }.freeze
 
-    paragraph_types = [:plain, :emailquote, :codeblock, :header, :footer]
-    paragraph_rules = {
-      Proc.new { |line| line.start_with? "&gt; " } => lambda { |paragraph|
-        paragraph.type = :emailquote },
-      Proc.new { |line| line.start_with? "    " } => lambda { |paragraph|
-        paragraph.type = :codeblock },
-      Proc.new { |line| /^\[\d+\]: / =~ line } => lambda do |paragraph|
-        paragraph.content.gsub!(%r{^(\[\d+\]:) (.*://(.*))}) do
-          "#{$1} <a href=\"#{$2}\">#{$3[0, 64]}#{%{...} if $3.length > 67}</a>"
-        end
-        paragraph.type = :footer
-      end
-    }.freeze
-
     # First, htmlescape the body text
     body.gsub!(/[&"'<>]/, htmlescape_rules)
 
+    # Split the body into Paragraphs
     paragraphs = []
     body.split("\n\n").each do |content|
       paragraphs << Paragraph.new(content)
     end
 
+    # These are the values that Paragraph.type can take
+    paragraph_types = [:plain, :emailquote, :codeblock, :header, :footer]
+
+    # Special matching for the first paragraph.  This paragraph will
+    # be marked header:
+    #
+    # (This is a really long first paragraph
+    # that spans to two lines)
     paragraphs[0].first = true
     if paragraphs[0].content.start_with? "(" and
         paragraphs[0].content.end_with? ")"
       paragraphs[0].type = :header
     end
 
-    # Paragraph-level processing
+    # Key is used to match a paragraph, and value is the lambda
+    # that'll act on it.
+    paragraph_rules = {
+
+      # If all the lines in a paragraph, begin with "> ", the
+      # paragraph is marked as an :emailquote
+      Proc.new { |line| line.start_with? "&gt; " } => lambda { |paragraph|
+        paragraph.type = :emailquote },
+
+      # If all the lines in a paragraph, begin with "   ", the paragraph is
+      # marked as an :coeblock
+      Proc.new { |line| line.start_with? "    " } => lambda { |paragraph|
+        paragraph.type = :codeblock },
+
+      # If all the lines in a paragraph, begin with " ", the paragraph
+      # is marked as :footer.  Also, a regex subsittuion runs on each
+      # line turning every link like http://a-url-over-67-characters
+      # to <a href="http://google.com">64-characters-of-the-li...</a>
+      Proc.new { |line| /^\[\d+\]: / =~ line } => lambda do |paragraph|
+        paragraph.type = :footer
+        paragraph.content.gsub!(%r{^(\[\d+\]:) (.*://(.*))}) do
+          "#{$1} <a href=\"#{$2}\">#{$3[0, 64]}#{%{...} if $3.length > 67}</a>"
+        end
+      end
+    }.freeze
+
+    # Apply the paragraph_rules on all the paragraphs
     paragraphs.each do |paragraph|
       paragraph_rules.each do |proc_match, lambda_cb|
         if paragraph.content.lines.all? &proc_match
@@ -61,12 +90,16 @@ module ClayText
 
     # Generate is_*? methods for Paragraph
     Paragraph.class_eval do
-      paragraph_types.each do |type|
+      (paragraph_types + [:first]).each do |type|
         define_method("is_#{type.to_s}?") { @type == type }
       end
     end
 
+    # body is the useless version.  If someone is too lazy to xuse all
+    # the paragraphs individually in their template, they can just use
+    # this.
     body = paragraphs.map(&:content).join("\n\n")
+    
     paragraphs
   end
 end

lib/clayoven.rb
@@ -6,6 +6,9 @@ require 'clayoven/claytext'
 require 'clayoven/httpd'
 require 'clayoven/imapd'
 
+# Figures out the timestamp of the commit that introduced a specific
+# file.  If the file hasn't been checked into git yet, return the
+# current time.
 def when_introduced(filename)
   timestamp = `git log --reverse --pretty="%at" #{filename} 2>/dev/null | head -n 1`.strip
   if timestamp == ""
@@ -20,6 +23,9 @@ module Clayoven
     attr_accessor :filename, :permalink, :timestamp, :title, :topic, :body,
     :paragraphs, :target, :indexfill, :topics
 
+    # Writes out HTML pages.  Takes a list of topics to render
+    #
+    # Prints a "[GEN]" line for every file it writes out.
     def render(topics)
       @topics = topics
       @paragraphs = ClayText.process! @body
@@ -67,24 +73,32 @@ module Clayoven
       config.ignore.any? { |pattern| %r{#{pattern}} =~ entry }
     end
 
-    abort "error: design/template.slim file not found; aborting" if not
-      Dir.entries("design").include? "template.slim"
+    # We must have a "design" directory.  I don't plan on making this
+    # a configuration variable.
+    if not Dir.entries("design").include? "template.slim"
+      abort "error: design/template.slim file not found; aborting"
+    end
 
+    # index_files are files ending in ".index" and "index"
+    # content_files are all other files (we've already applied ignore)
+    # topics is the list of topics.  We need it for the sidebar
     index_files = ["index"] + all_files.select { |file| /\.index$/ =~ file }
     content_files = all_files - index_files
     topics = index_files.map { |file| file.split(".index")[0] }.uniq
 
-    # Next, look for stray files
+    # Look for stray files.  All content_files that don't have a valid
+    # topic before ":" (or don't have ";" in their filename at all)
     (content_files.reject { |file| topics.include? (file.split(":", 2)[0]) })
       .each do |stray_entry|
       content_files = content_files - [stray_entry]
       puts "warning: #{stray_entry} is a stray file or directory; ignored"
     end
 
+    # Turn index_files and content_files into objects
     index_pages = index_files.map { |filename| IndexPage.new(filename) }
     content_pages = content_files.map { |filename| ContentPage.new(filename) }
 
-    # First, fill in all the page attributes
+    # Fill in page.title and page.body by reading the file
     (index_pages + content_pages).each do |page|
       page.title, page.body = (IO.read page.filename).split("\n\n", 2)
     end

lib/clayoven/config.rb
@@ -8,6 +8,9 @@ module Clayoven
       @rootpath = ".clayoven"
       @rcpath = File.expand_path "~/.clayovenrc"
       @ignorepath = "#{rootpath}/ignore"
+
+      # Most common patterns that should sit in .clayoven/ignore.
+      # Written to the file when it doesn't exist.
       @ignore = ["\\.html$", "~$", "^.\#", "^\#.*\#$",
                  "^\\.git$", "^\\.gitignore$", "^\\.htaccess$"]
       @rc = nil

lib/clayoven/httpd.rb
@@ -5,6 +5,11 @@ module Clayoven
     def self.start
       port = 8000
       callback = Proc.new do |req, res|
+
+        # A couple of URL rewriting rules.  Not real URL rewriting
+        # like .htaccess; just a HTTP redirect. / is rewritten to
+        # index.html, and anything-without-a-period is rewritten to
+        # that-thing.html.
         if %r{^/$} =~ req.path_info
           res.set_redirect WEBrick::HTTPStatus::Found, "index.html"
         end

lib/clayoven/imapd.rb
@@ -2,7 +2,13 @@ require 'net/imap'
 require_relative 'config'
 
 module Clayoven
+  # `clayoven impad` essentially calls Imapd.poll
+  # (but also calls Clayoven.main)
   module Imapd
+    # Initialites a connection to the IMAP server, and fetches new
+    # messages.
+    #
+    # Returns an unnamed Struct with :filename, :date, :msgid fields
     def self.poll
       config = Clayoven::ConfigData.new
       abort "error: #{config.rcpath} not found; aborting" if not config.rc
@@ -15,6 +21,9 @@ module Clayoven
       server.examine "INBOX"
       server.search(["ALL"]).each do |id|
         message = server.fetch(id, ["ENVELOPE", "RFC822.TEXT"])[0]
+
+        # This block is only run if we receive email from the trusted
+        # sender (a configuration variable).
         trustmailbox, trusthost = config.rc["trustfrom"].split("@")
         if message.attr["ENVELOPE"].sender[0].mailbox == trustmailbox and
             message.attr["ENVELOPE"].sender[0].host == trusthost

commit 0a4510c1821a1b9eb5c9001867981e0601f7509c

claytext: fix Paragraph.is_first?
8b0cea8 (*: comments along with minor cleanup, 2013-02-18) introduced
a regression where Paragraph.is_first? wasn't being properly defined.
Fix this.

lib/clayoven/claytext.rb
@@ -25,6 +25,10 @@ module ClayText
       @first = false
       @type = :plain
     end
+
+    def is_first?
+      @first
+    end
   end
 
   # Takes a body of claytext, breaks it up into paragraphs, and
@@ -90,7 +94,7 @@ module ClayText
 
     # Generate is_*? methods for Paragraph
     Paragraph.class_eval do
-      (ClayText::PARAGRAPH_TYPES + [:first]).each do |type|
+      ClayText::PARAGRAPH_TYPES.each do |type|
         define_method("is_#{type.to_s}?") { @type == type }
       end
     end