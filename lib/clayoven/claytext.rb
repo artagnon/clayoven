042ebf907c8ad01c4c093a80953b31eb8c2bfe65
Ramkumar Ramachandra <artagnon@gmail.com>
2013-04-04 02:02:57 +0530

    claytext: introduce ^two-spaces for :listitem
    Apart from the vertical inter-item spacing (which I really don't care
    about), a list item simply introduces a left margin.  Let's just
    emulate this when each line in a paragraph matches ^two-spaces.

--- lib/clayoven/claytext.rb
+++ lib/clayoven/claytext.rb
@@ -1,6 +1,6 @@
 module ClayText
   # These are the values that Paragraph.type can take
-  PARAGRAPH_TYPES = %i[plain emailquote codeblock subheading header footer]
+  PARAGRAPH_TYPES = %i[plain emailquote codeblock listitem subheading header footer]
 
   # see: http://php.net/manual/en/function.htmlspecialchars.php
   HTMLESCAPE_RULES = {
@@ -27,6 +27,13 @@ module ClayText
     Proc.new { |line| line.start_with? "    " } => lambda { |paragraph|
       paragraph.content = paragraph.content.lines.map { |l| l[4..-1] }.join
       paragraph.type = :codeblock },
+
+    # If all the lines in a paragraph begin with "  ", those two
+    # characters are stripped from the content, and the paragraph is
+    # marked as an :listitem,
+    Proc.new { |line| line.start_with? "  " } => lambda { |paragraph|
+      paragraph.content = paragraph.content.lines.map { |l| l[2..-1] }.join
+      paragraph.type = :listitem },
 
     # If all the lines in a paragraph begin with " ", the paragraph
     # is marked as :footer.  Also, a regex substitution runs on each
