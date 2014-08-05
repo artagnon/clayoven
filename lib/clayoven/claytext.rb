476ee718e71cdc6d91aa6cb29ab64944bf785b62
Ramkumar Ramachandra <artagnon@gmail.com>
2014-08-05 13:12:00 -0400

    claytext: remove useless paragraph.body member

--- lib/clayoven/claytext.rb
+++ lib/clayoven/claytext.rb
@@ -172,11 +172,6 @@ module ClayText
       end
     end
 
-    # body is the useless version.  If someone is too lazy to use all
-    # the paragraphs individually in their template, they can just use
-    # this.
-    body = paragraphs.map(&:content).join("\n\n")
-
     paragraphs
   end
 end
