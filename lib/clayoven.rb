733c5a7b02363e28945c5f82f1fdf7af83740302
Ramkumar Ramachandra <artagnon@gmail.com>
2019-03-23 00:00:59 +0530

    [clayoven] Regenerate with dep info, major speedup
    Sitemap creation is broken, because that would require all Page
    objects to be built; we plan to introduce:
    
    	$ clayoven all
    
    For an aggressive regeneration.

--- lib/clayoven.rb
+++ lib/clayoven.rb
@@ -22,19 +22,50 @@ def ls_files config
 end
 
 module Clayoven
+  class Git
+    def initialize
+      git_ns = `git diff --name-status @`
+      if not git_ns.empty? then
+        git_index = git_ns.split("\n").map { |line| line.split("\t")[0..1] }
+        git_mod_index = git_index.select { |idx| idx.first == "M" }
+        @modified = git_mod_index.map { |idx| idx.last }
+        @added = (git_index - git_mod_index).map { |idx| idx.last }
+      else
+        @modified = []
+        @added = []
+      end
+    end
+
+    def modified? file
+      @modified.include? file
+    end
+
+    def added? file
+      @added.include? file
+    end
+
+    def added_or_modified? file
+      added?(file) || modified?(file)
+    end
+
+    def auth_pub_dates file
+      dates = `git log --follow --format="%aD" --date=unix #{file}`.split "\n"
+      pubdate = if added_or_modified? file then
+        Time.now else Time.parse dates.first
+      end
+      return pubdate, Time.parse(dates.last)
+    end
+  end
+
   class Page
-    attr_accessor :filename, :permalink, :timestamp, :title, :topic, :body,
-    :pubdateobj, :authdateobj, :paragraphs, :target, :indexfill, :topics
+    attr_accessor :filename, :permalink, :title, :topic, :body, :pubdate, :authdate, :paragraphs, :target, :topics, :indexfill
 
     # Intialize with filename and authored dates from git
     def initialize filename, gidx
       @filename = filename
-      @dates = `git log --follow --format="%aD" --date=unix #{@filename}`.split "\n"
-      pubdate = @dates.first
       # If a file is in the git index, use Time.now; otherwise, log --follow it.
-      @pubdateobj = if gidx.include? @filename then Time.now else Time.parse(pubdate) end
-      authdate = @dates.last
-      @authdateobj = Time.parse(authdate)
+      @pubdate, @authdate = gidx.auth_pub_dates @filename
+      @title, @body = (IO.read @filename).split "\n\n", 2
     end
 
     # Writes out HTML pages.  Takes a list of topics to render
@@ -68,29 +99,41 @@ module Clayoven
   class ContentPage < Page
     def initialize filename, gidx
       super
+      # There cannot be ContentPages nested under 'index'
       @topic, _ = @filename.split '/', 2
       @target = "#{@filename}.html"
       @permalink = @filename
-      @indexfill = nil
     end
   end
 
-  # Populate the indexfill field in each topic
-  def self.indexfill topics, index_pages, content_pages
-    topics.each do |topic|
-      topic_index = index_pages.select { |page| page.topic == topic }.first
-      topic_index.indexfill = content_pages.select { |page| page.topic == topic }
-    end
-  end
-
-  def self.page_objects index_files, content_files
+  # Return index_pages and content_pages to generate
+  def self.pages_to_regenerate index_files, content_files
     # Check the git index exactly once to determine dirty files
-    gidx = `git diff --name-only @`.split "\n"
-    index_pages = (lex_sort index_files).map { |filename| IndexPage.new filename, gidx }
-    content_pages = content_files
+    gidx = Git.new
+
+    # Find out the dirty content_pages
+    dirty_content_pages = content_files
+      .select { |filename| gidx.added_or_modified? filename }
       .map { |filename| ContentPage.new filename, gidx }
-      .sort_by { |cp| [-cp.authdateobj.to_i, cp.filename] }
-    return index_pages, content_pages
+
+    # First, see which index_pages are forced to be dirty by corresponding content_pages;
+    # then, add to the list the ones that are dirty by themselves
+    dirty_index_pages = dirty_content_pages.map do |dcp|
+      IndexPage.new "#{dcp.topic}.index", gidx
+    end
+    dirty_index_pages += index_files
+      .select { |filename| gidx.added_or_modified? filename }
+      .map { |filename| IndexPage.new filename, gidx }
+
+    # Now, set the indexfill for index_pages by looking at all the content_files
+    # corresponding to a dirty index_page.
+    dirty_index_pages.each do |dip|
+      dip.indexfill = content_files
+        .select { |cf| cf.split('/', 2).first == dip.topic }
+        .map { |cf| ContentPage.new cf, gidx }
+        .sort_by { |cp| [-cp.authdate.to_i, cp.filename] }
+    end
+    return dirty_index_pages + dirty_content_pages
   end
 
   def self.generate_sitemap all_pages
@@ -98,7 +141,7 @@ module Clayoven
     SitemapGenerator::Sitemap.public_path = '.'
     SitemapGenerator::Sitemap.create do
       all_pages.each do |page|
-        add page.permalink, :lastmod => page.pubdateobj
+        add page.permalink, :lastmod => page.pubdate
       end
     end
   end
@@ -120,35 +163,19 @@ module Clayoven
     # topics is the list of topics.  We need it for the sidebar
     index_files = ['index', '404'] + all_files.select { |file| /\.index$/ =~ file }
     content_files = all_files - index_files
-    topics = (index_files - ['404']).map { |file| file.split('.index').first }
+    topics = lex_sort(index_files - ['404']).map { |file| file.split('.index').first }
 
     # Look for stray files.  All content_files are nested within directories
     content_files
-      .reject { |file| topics.include? file.split('/', 2)[0] }
+      .reject { |file| topics.include? file.split('/', 2).first }
       .each do |stray|
       content_files = content_files - [stray]
       puts "[WARN] #{stray} is a stray file or directory; ignored"
     end
 
-    # Turn index_files and content_files into objects
-    index_pages, content_pages = page_objects index_files, content_files
-
-    # Update topics to be a sorted Array extracted from index_pages.
-    topics = index_pages.reject { |page| page.topic == '404' }.map { |page| page.topic }
-
-    # Compute the indexfill for indexes
-    indexfill topics, index_pages, content_pages
-
-    # Operations on all_pages follow
-    all_pages = index_pages + content_pages
-
-    # Set the title and body for the render function
-    all_pages.each do |page|
-      page.title, page.body = (IO.read page.filename).split "\n\n", 2
-    end
-
-    # Produce the final HTML using slim
-    all_pages.each { |page| page.render topics }
-    generate_sitemap all_pages
+    # Get a list of pages to regenerate, and produce the final HTML using slim
+    genpages = pages_to_regenerate index_files, content_files
+    genpages.each { |page| page.render topics }
+    # generate_sitemap all_pages
   end
 end
