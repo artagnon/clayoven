5a98f597466b89ff4a7e3dfe2bcd4f09c46757e6
Ramkumar Ramachandra <artagnon@gmail.com>
2020-09-09 18:20:43 +0200

    [claytext] Add regexes for <{mark, a}>

--- lib/clayoven/claytext.rb
+++ lib/clayoven/claytext.rb
@@ -203,9 +203,11 @@ module ClayText
       p.gsub!(/[<>&]/, ClayText::HTMLESCAPE_RULES)
     end
 
-    # Insert HTML breaks in :plain paragraphs
-    paragraphs.filter { |p| p.type == :plain }.each do |p|
-      p.gsub!(/\n/, "<br/>\n")
+    # Insert HTML breaks, links, and marks in :plain and :olitems paragraphs
+    paragraphs.filter { |p| p.type == :plain or p.type == :olitems }.each do |p|
+      p.gsub! /\n/, "<br/>\n"
+      p.gsub! /`([^`]+)`/, '<mark>\1</mark>'
+      p.gsub! /\[([^\]]+)\]\(([^)]+)\)/, '<a href="\2">\1</a>'
     end
 
     paragraphs
