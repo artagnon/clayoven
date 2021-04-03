8b240e50be534cb36f58ea44af155378b66a3468
Ramkumar Ramachandra <r@artagnon.com>
2021-04-03 13:07:51 +0200

    [dist] Update with rake cpdist

--- dist/lib/mathjax.js
+++ dist/lib/mathjax.js
@@ -1,46 +1,49 @@
-import { readFileSync, writeFileSync } from 'fs';
-import { argv } from 'yargs';
-import 'colors';
-const ProgressBar = require('progress');
+import { readFileSync, writeFileSync } from "fs";
+import { argv } from "yargs";
+import "colors";
+const ProgressBar = require("progress");
 
-import * as mathjax from 'mathjax-full';
+import * as mathjax from "mathjax-full";
 
-mathjax.init({
+mathjax
+  .init({
     options: {
-        typesetError: (_, math, err) => console.log(math.math + ": " + err)
+      typesetError: (_, math, err) => console.log(math.math + ": " + err),
     },
     loader: {
-        paths: { mathjax: 'mathjax-full/es5', custom: '.' },
-        require: require,
-        load: ['input/tex-full', 'output/chtml', '[custom]/xypic'],
+      paths: { mathjax: "mathjax-full/es5", custom: "." },
+      require: require,
+      load: ["input/tex-full", "output/chtml", "[custom]/xypic"],
     },
     tex: {
-        packages: { '[+]': ['xypic'] },
-        inlineMath: [['$', '$']]
+      packages: { "[+]": ["xypic"] },
+      inlineMath: [["$", "$"]],
     },
     chtml: {
-        fontURL: 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/output/chtml/fonts/woff-v2'
-    }
-}).then((MathJax) => {
-    let progress = new ProgressBar(`[${'TeX'.green} ]: |:bar| :current/:total`, {
-        total: argv._.length,
-        incomplete: ' '
+      fontURL: "https://cdn.jsdelivr.net/npm/mathjax@3/es5/output/chtml/fonts/woff-v2",
+    },
+  })
+  .then((MathJax) => {
+    let progress = new ProgressBar(`[${"TeX".green} ]: |:bar| :current/:total`, {
+      total: argv._.length,
+      incomplete: " ",
     });
-    argv._.forEach(r => {
-        // Read in the HTML file
-        const html = MathJax.startup.document = MathJax.startup.getDocument(readFileSync(r, 'utf8'));
+    argv._.forEach((r) => {
+      // Read in the HTML file
+      const html = (MathJax.startup.document = MathJax.startup.getDocument(readFileSync(r, "utf8")));
 
-        // xypic has used the adaptor
-        const adaptor = MathJax.startup.adaptor;
+      // xypic has used the adaptor
+      const adaptor = MathJax.startup.adaptor;
 
-        // Clear the font cache
-        html.outputJax.font.clearCache();
+      // Clear the font cache
+      html.outputJax.clearCache();
 
-        // Typeset the document, with the render hooks that xypic has put in place
-        html.clear().render();
+      // Typeset the document, with the render hooks that xypic has put in place
+      html.clear().render();
 
-        // Output the resulting HTML in-place
-        writeFileSync(r, adaptor.doctype(html.document) + adaptor.outerHTML(adaptor.root(html.document)));
-        progress.tick();
-    })
-}).catch(err => console.log(err));
+      // Output the resulting HTML in-place
+      writeFileSync(r, adaptor.doctype(html.document) + adaptor.outerHTML(adaptor.root(html.document)));
+      progress.tick();
+    });
+  })
+  .catch((err) => console.log(err));
