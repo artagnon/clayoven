09fa3ad20d308c525f2cbab306a390c758ed2d20
Ramkumar Ramachandra <r@artagnon.com>
2021-04-04 15:18:17 +0200

    [coverage] Try instrumenting fork
    Doesn't seem to work.

--- test/helper.rb
+++ test/helper.rb
@@ -1,4 +1,10 @@
 require 'simplecov'
-SimpleCov.start
+
+SimpleCov.start do
+  command_name 'minitest'
+  enable_coverage :branch
+  primary_coverage :branch
+  enable_for_subprocesses true
+end
 
 require 'minitest/autorun'
